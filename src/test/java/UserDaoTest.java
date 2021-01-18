d3f43e525d564cfbbadba3bbdaebed11ba9fae62
kyunam Sim <ppp9026@naver.com>
2021-01-18 23:16:17 +0900

    토비 스프링 2강 연습 (테스트) - UserDao 테스트 코드 addGet 작성

--- src/test/java/UserDaoTest.java
+++ src/test/java/UserDaoTest.java
@@ -25,16 +25,44 @@ class UserDaoTest {
 //        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 
         UserDao dao = context.getBean("userDao", UserDao.class);
-        User user = new User();
-        user.setId("gyumee");
-        user.setName("박성철");
-        user.setPassword("springno1");
+        User user1 = new User("gyumee", "박성철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
 
-        dao.add(user);
+        dao.deleteAll();
+        assertThat(dao.getCount(), equalTo(0));
 
-        User user2 = dao.get(user.getId());
-        assertThat(user2.getName(), equalTo(user.getName()));
-        assertThat(user2.getPassword(), equalTo(user.getPassword()));
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount(), equalTo(2));
 
+        User userget1 = dao.get(user1.getId());
+        assertThat(userget1.getName(), equalTo(user1.getName()));
+        assertThat(userget1.getPassword(), equalTo(user1.getPassword()));
+
+        User userget2 = dao.get(user2.getId());
+        assertThat(userget2.getName(), equalTo(user2.getName()));
+        assertThat(userget2.getPassword(), equalTo(user2.getPassword()));
+    }
+
+    @Test
+    public void count() throws SQLException{
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user1 = new User("gyumee", "박성철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
+        User user3 = new User("bumjin", "박범진", "springno3");
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), equalTo(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(), equalTo(1));
+
+        dao.add(user2);
+        assertThat(dao.getCount(), equalTo(2));
+
+        dao.add(user3);
+        assertThat(dao.getCount(), equalTo(3));
     }
 }
