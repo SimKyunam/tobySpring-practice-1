6d2842eced3ac0e2e43ebc1d4b710acdc28494ad
kyunam Sim <ppp9026@naver.com>
2021-01-18 22:09:57 +0900

    토비 스프링 2강 연습 (테스트) - UserDao 테스트 코드 addGet 작성

--- src/test/java/UserDaoTest.java
+++ src/test/java/UserDaoTest.java
@@ -1,10 +1,16 @@
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.test.context.support.GenericXmlContextLoader;
 import user.dao.DaoFactory;
+import user.dao.User;
 import user.dao.UserDao;
+
+import java.sql.SQLException;
 
 /**
  * Created by mileNote on 2021-01-15
@@ -12,12 +18,23 @@ import user.dao.UserDao;
  * Github : https://github.com/SimKyunam
  */
 class UserDaoTest {
-    public static void main(String[] args) {
+
+    @Test
+    public void addAndGet() throws SQLException {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 //        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao beanDao1 = context.getBean("userDao", UserDao.class);
-        UserDao beanDao2 = context.getBean("userDao", UserDao.class);
-        System.out.println(beanDao1);
-        System.out.println(beanDao2);
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user = new User();
+        user.setId("gyumee");
+        user.setName("박성철");
+        user.setPassword("springno1");
+
+        dao.add(user);
+
+        User user2 = dao.get(user.getId());
+        assertThat(user2.getName(), equalTo(user.getName()));
+        assertThat(user2.getPassword(), equalTo(user.getPassword()));
+
     }
 }
