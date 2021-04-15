ecb827c73997732a1ae6ea968bcc98c8462a5e63
kyunam Sim <ppp9026@naver.com>
2021-04-15 22:25:14 +0900

    토비 스프링 6강 연습 - 서비스 레이어에 트랜잭션 적용

--- src/main/java/user/service/UserService.java
+++ src/main/java/user/service/UserService.java
@@ -2,6 +2,8 @@ package user.service;
 
 import user.domain.User;
 
+import java.util.List;
+
 /**
  * Created by mileNote on 2021-03-16
  * Blog : https://milenote.tistory.com
@@ -9,5 +11,9 @@ import user.domain.User;
  */
 public interface UserService {
     void add(User user);
+    User get(String id);
+    List<User> getAll();
+    void deleteAll();
+    void update(User user);
     void upgradeLevels();
 }
--- src/main/java/user/service/UserServiceImpl.java
+++ src/main/java/user/service/UserServiceImpl.java
@@ -25,6 +25,26 @@ public class UserServiceImpl implements UserService{
         this.mailSender = mailSender;
     }
 
+    @Override
+    public User get(String id) {
+        return userDao.get(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userDao.getAll();
+    }
+
+    @Override
+    public void deleteAll() {
+        userDao.deleteAll();
+    }
+
+    @Override
+    public void update(User user) {
+        userDao.update(user);
+    }
+
     public void upgradeLevels(){
         List<User> users = userDao.getAll();
         for(User user: users){
