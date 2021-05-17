88f7806e75e1cc2e0adf0932af49150330e221c9
kyunam Sim <ppp9026@naver.com>
2021-05-17 22:36:38 +0900

    토비 스프링 7장 연습 - 7.3 OXM을 적용하여 SqlService 설정

---
+++ src/main/java/user/sqlservice/OxmSqlService.java
@@ -0,0 +1,82 @@
+package user.sqlservice;
+
+import org.springframework.oxm.Unmarshaller;
+import user.sqlservice.jaxb.SqlType;
+import user.sqlservice.jaxb.Sqlmap;
+
+import javax.annotation.PostConstruct;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * Created by mileNote on 2021-05-17
+ * Blog : https://milenote.tistory.com
+ * Github : https://github.com/SimKyunam
+ */
+public class OxmSqlService implements SqlService{
+    private final OxmSqlReader oxmSqlReader = new OxmSqlReader();
+    private SqlRegistry sqlRegistry = new HashMapSqlRegistry();
+
+    public void setSqlRegistry(SqlRegistry sqlRegistry) {
+        this.sqlRegistry = sqlRegistry;
+    }
+
+    public void setUnmarshaller(Unmarshaller unmarshaller) {
+        this.oxmSqlReader.setUnmarshaller(unmarshaller);
+    }
+
+    public void setSqlmapFile(String sqlmapFile) {
+        this.oxmSqlReader.setSqlmapFile(sqlmapFile);
+    }
+
+    @PostConstruct
+    public void loadSql(){
+        this.oxmSqlReader.read(this.sqlRegistry);
+    }
+
+    @Override
+    public String getSql(String key) throws SqlRetrievalFailureException {
+        try{
+            return this.sqlRegistry.findSql(key);
+        }catch (SqlNotFoundException e){
+            throw new SqlRetrievalFailureException(e.getMessage());
+        }
+    }
+
+    private class OxmSqlReader implements SqlReader{
+        private Unmarshaller unmarshaller;
+        private static final String DEFAULT_SQLMAP_FILE = "mapper/sqlmap.xml";
+        private String sqlmapFile = DEFAULT_SQLMAP_FILE;
+
+        public void setUnmarshaller(Unmarshaller unmarshaller) {
+            this.unmarshaller = unmarshaller;
+        }
+
+        public void setSqlmapFile(String sqlmapFile) {
+            this.sqlmapFile = sqlmapFile;
+        }
+
+        @Override
+        public void read(SqlRegistry sqlRegistry) {
+            try{
+                Source xmlSource = new StreamSource(
+                        getXmlFile("mapper/sqlmap.xml"));
+
+                Sqlmap sqlmap = (Sqlmap) this.unmarshaller.unmarshal(xmlSource);
+                for(SqlType sql : sqlmap.getSql()){
+                    sqlRegistry.registerSql(sql.getKey(), sql.getValue());
+                }
+            } catch (IOException e){
+                throw new RuntimeException(this.sqlmapFile + "을 가져올 수 없습니다.", e);
+            }
+        }
+
+        private File getXmlFile(String fileName) {
+            ClassLoader classLoader = getClass().getClassLoader();
+            return new File(classLoader.getResource(fileName).getFile());
+        }
+    }
+}
