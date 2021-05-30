26da1bdbb3ac4d475e83b9a5106e2ee820b93881
kyunam Sim <ppp9026@naver.com>
2021-05-30 23:50:14 +0900

    토비 스프링 7장 연습 - 7.5.2 내장형 데이터베이스

---
+++ src/main/java/issuetracker/sqlservice/EmbeddedDbSqlRegistry.java
@@ -0,0 +1,51 @@
+package issuetracker.sqlservice;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import user.sqlservice.SqlNotFoundException;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+/**
+ * Created by mileNote on 2021-05-29
+ * Blog : https://milenote.tistory.com
+ * Github : https://github.com/SimKyunam
+ */
+public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry{
+    JdbcTemplate jdbc;
+    public void setDataSource(DataSource dataSource){
+        jdbc = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public void registerSql(String key, String sql) {
+        jdbc.update("insert into sqlmap(key_, sql_) values(?, ?)", key, sql);
+    }
+
+    @Override
+    public String findSql(String key) throws SqlNotFoundException {
+        try{
+            return jdbc.queryForObject("select sql_ from sqlmap where key_ = ?", String.class, key);
+        }catch (EmptyResultDataAccessException e){
+            throw new SqlNotFoundException(key +
+                    "에 해당하는 SQL을 찾을 수 없습니다.", e);
+        }
+    }
+
+    @Override
+    public void updateSql(String key, String sql) throws SqlUpdateFailureException {
+        int affected = jdbc.update("update sqlmap set sql_ = ? where key_ = ?", sql, key);
+        if(affected == 0){
+            throw new SqlUpdateFailureException(key +
+                    "에 해당하는 SQL을 찾을 수 없습니다.");
+        }
+    }
+
+    @Override
+    public void updateSql(Map<String, String> sqlMap) throws SqlUpdateFailureException {
+        for(Map.Entry<String, String> entry : sqlMap.entrySet()){
+            updateSql(entry.getKey(), entry.getValue());
+        }
+    }
+}
--- src/test/java/issuetracker/ConcurrentHashMapSqlRegistryTest.java
+++ src/test/java/issuetracker/ConcurrentHashMapSqlRegistryTest.java
@@ -17,55 +17,9 @@ import static org.junit.jupiter.api.Assertions.*;
  * Blog : https://milenote.tistory.com
  * Github : https://github.com/SimKyunam
  */
-public class ConcurrentHashMapSqlRegistryTest {
-    UpdatableSqlRegistry sqlRegistry;
-
-    @BeforeEach
-    public void setUp(){
-        sqlRegistry = new ConcurrentHashMapSqlRegistry();
-        sqlRegistry.registerSql("KEY1", "SQL1");
-        sqlRegistry.registerSql("KEY2", "SQL2");
-        sqlRegistry.registerSql("KEY3", "SQL3");
-    }
-
-    @Test
-    public void find(){
-        checkFindResult("SQL1", "SQL2", "SQL3");
-    }
-
-    private void checkFindResult(String expected1, String expected2, String expected3) {
-       assertEquals(sqlRegistry.findSql("KEY1"), expected1);
-       assertEquals(sqlRegistry.findSql("KEY2"), expected2);
-       assertEquals(sqlRegistry.findSql("KEY3"), expected3);
-    }
-
-    @Test
-    public void unknownKey(){
-        assertThrows(SqlNotFoundException.class, ()->{
-           sqlRegistry.findSql("SQL9999!@#");
-        });
-    }
-
-    @Test
-    public void updateSingle(){
-        sqlRegistry.updateSql("KEY2", "Modified2");
-        checkFindResult("SQL1", "Modified2", "SQL3");
-    }
-
-    @Test
-    public void updateMulti(){
-        Map<String, String> sqlmap = new HashMap<String, String>();
-        sqlmap.put("KEY1", "Modified1");
-        sqlmap.put("KEY3", "Modified3");
-
-        sqlRegistry.updateSql(sqlmap);
-        checkFindResult("Modified1", "SQL2", "Modified3");
-    }
-
-    @Test
-    public void updateWithNotExistingKey(){
-        assertThrows(SqlUpdateFailureException.class, ()->{
-           sqlRegistry.updateSql("SQL9999!@#", "Modified2");
-        });
+public class ConcurrentHashMapSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
+    @Override
+    protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
+        return new ConcurrentHashMapSqlRegistry();
     }
 }
